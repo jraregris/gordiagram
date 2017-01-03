0fbca1b687ec2374f7d9369d5b6c3e0f51b25aa2
Oddmund Strømme <oddmund@oddmundo.com>
2017-01-03 14:51:43 +0100

    Update hooup-mockup.js

--- hooup-mockup.js
+++ hooup-mockup.js
@@ -1,29 +1,24 @@
-
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+ctx.font = "10px mono";
+
 ctx.textAlign = "right";
-ctx.strokeText("VOX 1 ", 50, 20+4);
-ctx.strokeText("VOX 2 ", 50, 30+4);
-ctx.strokeText("VOX 3 ", 50, 40+4);
+ctx.fillText("VOX 1", 50-2, 20 + 4);
+ctx.fillText("VOX 2", 50-2, 30 + 4);
+ctx.fillText("VOX 3", 50-2, 40 + 4);
 
 ctx.beginPath();
-ctx.moveTo(50,20);
+ctx.moveTo(50, 20);
 ctx.bezierCurveTo(125, 20, 125, 100, 200, 100);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.moveTo(50,20-4);
-ctx.lineTo(50+10,20);
-ctx.lineTo(50,20+4);
-ctx.fill();
-
-ctx.beginPath();
-ctx.moveTo(50,30);
+ctx.moveTo(50, 30);
 ctx.bezierCurveTo(125, 30, 125, 100, 200, 100);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.moveTo(50,40);
+ctx.moveTo(50, 40);
 ctx.bezierCurveTo(125, 40, 125, 100, 200, 100);
 ctx.stroke();
